705e5d15dae87c05f2e61a09f6a0a6b82b16734b
Krish-Goyani <goyani@duck.com>
2025-03-06 18:02:28 +0530

    settings added

--- src/app/config/settings.py
+++ src/app/config/settings.py
@@ -1,1 +1,16 @@
-# Placeholder for settings.py
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    PINECONE_API_KEY: str
+    OPENAI_KEY: str
+    GEMINI_API_KEY: str
+    INDEX_NAME: str
+    INDEX_HOST: str
+    JINA_API_KEY: str
+
+    class Config:
+        env_file = "src/.env"
+
+
+settings = Settings()
