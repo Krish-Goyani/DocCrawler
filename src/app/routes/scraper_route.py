db786e959651008d19281fe6a9fb86f3a26d97e1
Naman7214 <namanagnihotri2021.it@mmcoe.edu.in>
2025-03-07 12:53:51 +0530

    List added

--- src/app/routes/scraper_route.py
+++ src/app/routes/scraper_route.py
@@ -4,11 +4,12 @@ from fastapi import APIRouter, Depends
 
 from src.app.controllers.scrape_controller import ScrapeController
 from src.app.utils.error_handler import error_handler
+from typing import List
 
 router = APIRouter()
 
 
 @error_handler
 @router.post("/scrape")
-async def scrape(urls: list, scrape_controller=Depends(ScrapeController)):
+async def scrape(urls: List, scrape_controller=Depends(ScrapeController)):
     return await scrape_controller.scrape(str(uuid.uuid4()), urls)
