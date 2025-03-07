7be7bdc3446947d0ed150412fc13d4f6f5cca65c
Harsha Bajaj <harshabajaj@Harshas-MacBook-Pro.local>
2025-03-07 10:48:11 +0530

    Initial Configurations

---
+++ src/app/config/clients.py
@@ -0,0 +1,12 @@
+from openai import AsyncOpenAI
+from pinecone.grpc import PineconeGRPC as Pinecone
+
+from config.settings import settings
+
+
+class Clients:
+    def get_openai_client(self):
+        return AsyncOpenAI(api_key=settings.OPENAI_KEY)
+
+    def get_pinecone_client(self):
+        return Pinecone(api_key=settings.PINECONE_API_KEY, pool_threads=30)
